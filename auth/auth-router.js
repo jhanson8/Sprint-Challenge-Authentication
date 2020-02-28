1797096ec86020ee354fc9a1457e9c2ddee727db
Jonathan Hanson <jhanson8@uoregon.edu>
2020-02-28 08:53:39 -0800

    middleware and login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -22,7 +22,40 @@ router.post("/register", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-  // implement login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user); // get a token
+
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token // send the token
+        });
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      console.log("ERROR: ", error);
+      res.status(500).json({ error: "/login error" });
+    });
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+    // role: user.role || "user"
+  };
+
+  const options = {
+    expiresIn: "1h"
+  };
+
+  return jwt.sign(payload, jwtSecret, options);
+}
 
 module.exports = router;
--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,17 @@
 module.exports = {
   development: {
-    client: 'sqlite3',
-    connection: { filename: './database/auth.db3' },
+    client: "sqlite3",
+    connection: { filename: "./database/auth.db3" },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      }
+    },
     useNullAsDefault: true,
     migrations: {
-      directory: './database/migrations',
-      tableName: 'dbmigrations',
+      directory: "./database/migrations",
+      tableName: "dbmigrations"
     },
-    seeds: { directory: './database/seeds' },
-  },
+    seeds: { directory: "./database/seeds" }
+  }
 };
